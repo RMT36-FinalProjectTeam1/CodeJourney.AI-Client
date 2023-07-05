e21310df2bde53f7524e5f43e422e0f5eae8018e
Bhagaskoro P. Nindyalit <kurohakuro@gmail.com>
2023-07-05 17:01:25 +0700

    refactor: Dashboard Card
    refactor dashboard card adding function

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -5,51 +5,51 @@
         <h1>All Schedules</h1>
       </div>
       <div class="dashboard-v2-content">
-        <div class="schedule-card">
-          <h4>Schedule Name</h4>
-          <span>Estimated Time: 4 Months</span>
-          <hr />
-          <p>Total Tasks: 35</p>
-          <div class="progress-bar">
-            <div class="percentage">
-              <p>Goals Reached</p>
-              <p>54%</p>
+        <div class="schedule-card" v-for="(schedule, i) in schedules" key="i">
+          <RouterLink class="router-link" :to="`/schedule/${schedule._id}`">
+            <h4>{{ schedule.scheduleTitle }}</h4>
+            <span>Estimated Time : {{ getDuration(schedule.schedules) }}</span>
+            <hr />
+            <p>Total Tasks: {{ schedule.schedules.length }}</p>
+            <div class="progress-bar">
+              <div class="percentage">
+                <p>Goals Reached</p>
+                <p>{{ getProgress(schedule.schedules) }}</p>
+              </div>
+              <progress max="100" :value="getProgress(schedule.schedules)"></progress>
             </div>
-            <progress max="100" value="54"></progress>
-          </div>
-        </div>
-        <div class="schedule-card">
-          <h4>Schedule Name</h4>
-          <span>Estimated Time: 4 Months</span>
-          <hr />
-          <p>Total Tasks: 35</p>
-          <div class="progress-bar">
-            <div class="percentage">
-              <p>Goals Reached</p>
-              <p>54%</p>
-            </div>
-            <progress max="100" value="54"></progress>
-          </div>
-        </div>
-        <div class="schedule-card">
-          <h4>Schedule Name</h4>
-          <span>Estimated Time: 4 Months</span>
-          <hr />
-          <p>Total Tasks: 35</p>
-          <div class="progress-bar">
-            <div class="percentage">
-              <p>Goals Reached</p>
-              <p>54%</p>
-            </div>
-            <progress max="100" value="54"></progress>
-          </div>
+          </RouterLink>
         </div>
       </div>
     </div>
   </section>
 </template>
 
-<script></script>
+<script>
+import { mapState, mapWritableState } from 'pinia'
+import { useCounterStore } from '../stores/counter'
+export default {
+  name: 'Home',
+  computed: {
+    ...mapState(useCounterStore, ['schedules', 'lengthMonth']),
+    ...mapWritableState(useCounterStore, ['currentMonth'])
+  },
+  methods: {
+    getProgress(tasks) {
+      const maxTask = tasks.length
+      let completeTask = 0
+      tasks.forEach((el) => {
+        if (el.complete) completeTask++
+      })
+      return Math.ceil((completeTask / maxTask) * 100)
+    },
+    getDuration(tasks) {
+      const days = tasks.length
+      if (days < 24) return `${days} days`
+      else return `${(days - (days % 4)) / 4 + 1} weeks`
+    }
+  }
+}
+</script>
 
-<style scoped>
-</style>
+<style scoped></style>
